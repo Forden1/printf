7cb456c44042bba4f638a341b71d7d077a33ef70
Forden1 <142172201+Forden1@users.noreply.github.com>
2023-10-20 00:24:09 +0300

    a

--- _printf.c
+++ _printf.c
@@ -4,11 +4,7 @@ void cleanup(va_list args, buffer_t *output);
 int run_printf(const char *format, va_list args, buffer_t *output);
 int _printf(const char *format, ...);
 
-/**
- * cleanup - Peforms cleanup operations for _printf.
- * @args: A va_list of arguments provided to _printf.
- * @output: A buffer_t struct.
- */
+
 void cleanup(va_list args, buffer_t *output)
 {
 	va_end(args);
@@ -16,14 +12,7 @@ void cleanup(va_list args, buffer_t *output)
 	free_buffer(output);
 }
 
-/**
- * run_printf - Reads through the format string for _printf.
- * @format: Character string to print - may contain directives.
- * @output: A buffer_t struct containing a buffer.
- * @args: A va_list of arguments.
- *
- * Return: The number of characters stored to output.
- */
+
 int run_printf(const char *format, va_list args, buffer_t *output)
 {
 	int i, wid, prec, ret = 0;
@@ -64,12 +53,6 @@ int run_printf(const char *format, va_list args, buffer_t *output)
 	return (ret);
 }
 
-/**
- * _printf - Outputs a formatted string.
- * @format: Character string to print - may contain directives.
- *
- * Return: The number of characters printed.
- */
 int _printf(const char *format, ...)
 {
 	buffer_t *output;
